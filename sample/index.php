5710b71f29f28046cd6e7bb5a9c44b1065315f07
Rafael Becker <realfabecker@gmail.com>
2017-07-13 21:23:50 -0300

    [UPD] simplificado exemplo aplicacao

--- sample/index.php
+++ sample/index.php
@@ -3,22 +3,20 @@
 require '../vendor/autoload.php';
 
 use HighWay\Wrappers\SlimApp\SlimHighWay;
-use HighWay\Wrappers\SlimApp\SlimMiddleware;
 use Solis\Breaker\TException;
 
 try {
 
     include_once 'src/Includes/config.php';
 
-    $app = SlimHighWay::make();
+    $routes = json_decode(
+        file_get_contents('src/Routes/sample.json'),
+        true
+    );
 
-    include_once 'src/Includes/dependencies.php';
-
-    $middleware = require_once 'src/Includes/middleware.php';
-
-    $app->getWrapper()->setMiddleware(SlimMiddleware::make($middleware));
-
-    $app->getWrapper()->compileRouteFromString(file_get_contents('src/Routes/sample.json'));
+    $app = SlimHighWay::make(
+        $routes
+    );
 
     $app->run();
 
